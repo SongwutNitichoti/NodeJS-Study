c867822e0b20601f9253ed82a9fcb22865341be5
Songwut Nitichoti <yeen9965@gmail.com>
2024-02-29 19:40:02 +0700

    Update Study

--- app.js
+++ app.js
@@ -5,18 +5,26 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 4000;
 
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")))
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get('/',(req,res) =>{
+app.set("views", "./src/views");
+app.set("view engine", "ejs")
 
-res.send('Hello World');
+
+app.get('/', (req, res) => {
+
+    res.render('index', { 
+        username: 'Yeen', 
+        customer: ["Yeen", "Mid", "Zeen", "Mom"],
+        title : 'Home Page' 
+    });
 
 });
 
-app.listen(port , () => {
-    console.log("Listening on port : "+chalk.yellow("http://localhost:"+port));
+app.listen(PORT, () => {
+    console.log("Listening on PORT : " + chalk.yellow("http://localhost:" + PORT));
 })
